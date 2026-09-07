6d2a14c49b1ef9f11c36a1da2c4b471dabea8a13
Nellaiappan-ig19 <nellai1661@gmail.com>
2026-09-07 10:39:14 +0530

    Create base Spring Boot project

---
+++ src/main/java/com/training/employeemanagementtraining/EmployeeManagementTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.training.employeemanagementtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/employeemanagementtraining/EmployeeManagementTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.employeemanagementtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
